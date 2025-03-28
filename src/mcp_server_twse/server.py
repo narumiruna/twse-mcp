b13f3467b8f47aead351841c03e6c7c2a679cdbb
narumi <toucans-cutouts0f@icloud.com>
2025-03-28 23:39:32 +0800

    use fastmcp

--- src/mcp_server_twse/server.py
+++ src/mcp_server_twse/server.py
@@ -1,54 +1,25 @@
-import anyio
-import mcp.server.stdio
-import mcp.types as types
-from mcp.server.lowlevel import Server
+from typing import Annotated
+
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
 
 from .twse import query_stock_info
 
-server: Server = Server("MCP Server TWSE")
+# https://github.com/jlowin/fastmcp/issues/81#issuecomment-2714245145
+server = FastMCP("MCP Server TWSE", log_level="ERROR")
 
 
-@server.list_tools()
-async def list_tools() -> list[types.Tool]:
-    return [
-        types.Tool(
-            name="query_stock_info",
-            description="Query stock information from TWSE",
-            inputSchema={
-                "type": "object",
-                "required": ["symbol"],
-                "properties": {
-                    "symbol": {
-                        "type": "string",
-                        "description": "The Taiwan stock symbol to query. e.q. 2330",
-                    }
-                },
-            },
-        )
-    ]
-
-
-@server.call_tool()
-async def query_tool(
-    name: str, arguments: dict
-) -> list[types.TextContent | types.ImageContent | types.EmbeddedResource]:
-    if name != "query_stock_info":
-        raise ValueError(f"Unknown tool: {name}")
-    if "symbol" not in arguments:
-        raise ValueError("Missing required argument 'symbol'")
-
-    result = query_stock_info(arguments["symbol"]).pretty_repr()
-    return [types.TextContent(type="text", text=result)]
-
-
-async def run() -> None:
-    async with mcp.server.stdio.stdio_server() as (read_stream, write_stream):
-        await server.run(
-            read_stream,
-            write_stream,
-            server.create_initialization_options(),
-        )
+@server.tool()
+async def query_stock_info_from_twse(
+    symbol: Annotated[str, Field(description="The Taiwan stock symbol to query. e.q. 2330")],
+) -> str:
+    """Query stock information from TWSE."""
+    try:
+        result = query_stock_info(symbol)
+        return result.pretty_repr()
+    except Exception as e:
+        return f"Error occurred while querying stock information: {str(e)}"
 
 
 def main() -> None:
-    anyio.run(run)
+    server.run()
