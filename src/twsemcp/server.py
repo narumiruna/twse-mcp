cfbb8b4d063576f44833847f24eb796ddb5b3e93
narumi <toucans-cutouts0f@icloud.com>
2025-04-10 12:36:59 +0800

    Refactor code structure for improved readability and maintainability

--- src/twsemcp/server.py
+++ src/twsemcp/server.py
@@ -1,9 +1,8 @@
 from typing import Annotated
 
+import twse
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
-
-from twsemcp.twse import StockInfoRequest
 
 # https://github.com/jlowin/fastmcp/issues/81#issuecomment-2714245145
 server = FastMCP("TWSE MCP Server", log_level="ERROR")
@@ -12,15 +11,11 @@ server = FastMCP("TWSE MCP Server", log_level="ERROR")
 @server.tool()
 async def get_stock_info(
     symbols: Annotated[list[str], Field(description="List of stock symbols to query, e.g., ['2330', '2317']")],
-    full_info: Annotated[bool, Field(description="Whether to return full information or not.")] = True,
 ) -> str:
     """Get stock information from TWSE."""
     try:
-        result = await StockInfoRequest(symbols=symbols).do()
-        if full_info:
-            return result.model_dump_json()
-        else:
-            return result.pretty_repr()
+        result = await twse.stock_info.async_get_stock_info(symbols)
+        return result.model_dump_json()
     except Exception as e:
         return f"Error occurred while querying stock information: {str(e)}"
 
