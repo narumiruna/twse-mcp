8ba59e87f60af360c9aac906857f86ce3940bd27
narumi <toucans-cutouts0f@icloud.com>
2025-04-29 21:35:39 +0800

    fix test case

--- tests/test_server.py
+++ tests/test_server.py
@@ -19,9 +19,9 @@ async def test_get_stock_info(server_params: StdioServerParameters) -> None:
         assert len(tools.tools) > 0
         assert tools.tools[0].name == "get_stock_info"
 
-        symbols = ["2330"]
-        result = await session.call_tool("get_stock_info", {"symbols": symbols, "full_info": True})
+        symbol = "2330"
+        result = await session.call_tool("get_stock_info", {"symbol": "2330", "full_info": True})
 
         json_data = "\n".join([t.text for t in result.content if isinstance(t, TextContent)])
         data = StockInfoResponse.model_validate_json(json_data, by_name=True)
-        assert data.msg_array[0].symbol == symbols[0]
+        assert data.msg_array[0].symbol == symbol
